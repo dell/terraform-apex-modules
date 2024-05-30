227e6956d68f878409d0d7b5881cedfb45d22e91
cdoria <connor.doria@dell.com>
2024-05-30 13:33:25 -0400

    Generate Docs, Move provider out of module into example

--- modules/apex-aws-account/variables.tf
+++ modules/apex-aws-account/variables.tf
@@ -1,12 +1,18 @@
-variable "aws_config_path" {
-  type = string
-  description = "Path to the AWS config file on your local machine check https://docs.aws.amazon.com/cli/latest/userguide/cli-configure-files.html for more information"
-}
+/*
+Copyright (c) 2024 Dell Inc., or its subsidiaries. All Rights Reserved.
 
-variable "aws_credentials_path" {
-  type = string
-  description = "Path to the AWS credentials file on your local machine check https://docs.aws.amazon.com/cli/latest/userguide/cli-configure-files.html for more information"
-}
+Licensed under the Mozilla Public License Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+	http://mozilla.org/MPL/2.0/
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
 
 variable "aws_account_id" {
   type = string
@@ -23,10 +29,4 @@ variable "aws_role_name" {
   type = string
   description = "The name of the role you would like to connect to APEX, defaults to APEX_AWS_ROLE"
   default = "APEX_AWS_ROLE"
-}
-
-variable "apex_host" {
-  type = string
-  description = "The instance of the Apex Navigator, defaults https://apex.apis.dell.com/apex and does not need to be changed in production environments"
-  default = "https://apex.apis.dell.com/apex"
 }
